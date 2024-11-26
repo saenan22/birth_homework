82d388b05b0b445e09cda0b5da578e8f9cdc6b26
saenan22 <yellow902@g.hongik.ac.kr>
2024-11-26 11:07:06 +0900

    Update geo_data_download.py

--- geo_data_download.py
+++ geo_data_download.py
@@ -1,6 +1,6 @@
 # Google Drive 파일 ID로 다운로드 URL 생성
 file_id = "164ypqgBqDYRChtcmTCI1daGQXZeNBw4d"
-download_url = f"https://drive.google.com/uc?export=download&id={file_id}"
+download_url = f"https://drive.google.com/uc?export=download&id={file_id}&confirm=t"
 
 # 파일 다운로드
 response = requests.get(download_url)
@@ -9,6 +9,4 @@ file_content = BytesIO(response.content)
 # GeoPandas로 파일 읽기
 gdf = gpd.read_file(file_content)
 
-# 데이터 출력 (선택 사항)
-print(gdf.head())
 
